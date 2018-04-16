35553a2fb25da4adba580966e0b72a776eef5a29
Hartox3 <38433605+Hartox3@users.noreply.github.com>
2018-04-16 18:20:20 +0300

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,12 @@
+import { endianness } from "os";
+
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on ("ready", function() {
+    bot.user.setGame("HmZ-Bot,  !help");
+     Console.log("Le HmZ-Bot a Bien Ete Connecter")
+});
+
+Bot.login("twkLBxCXkGhTIRAv5eYPQGpJW8-DwcLz");
